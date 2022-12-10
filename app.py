d0278280d18c7287ab894d75a234193eb9acd25b
MehakKambo <mehakpreetp12@gmail.com>
2022-12-09 18:42:19 -0800

    Download feature done

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from pytube import YouTube
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your key"
@@ -16,6 +17,17 @@ def home():
         return render_template("download.html", url = url)
     return render_template("home.html")
 
+@app.route("/download", methods = ["GET", "POST"])
+def download_video():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        itag = request.form.get("itag")
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name="Video.mp4", mimetype="video/mp4")
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
