ce2118dd7b6b44be36c441b338a0ef7a00890b21
MehakKambo <mehakpreetp12@gmail.com>
2022-12-09 18:31:13 -0800

    Backend for homepage done

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+from pytube import YouTube
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Your key"
 
-@app.route("/")
-def hello():
-    return "<h1> Testing Flask APP</h1>"
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+        except:
+            return render_template("error.html")
+        return render_template("download.html", url = url)
+    return render_template("home.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
